f90b462e5c644a6a690ac0290f7b7df0b6c071fe
Jaewon <choijawon99@gmail.com>
2024-01-11 16:13:52 +0900

    [FEAT] 1/11 문제풀이

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,64 +1,45 @@
 package org.example;
+
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        int testCase = sc.nextInt();
 
-        int[] arr = new int[N];
+        for(int i = 0;i<testCase;i++){
+            //문서의 개수
+            int N = sc.nextInt();
+            //문서의 위치
+            int M = sc.nextInt();
+            int turn = 1;
 
-        for(int i=0;i<N;i++){
-            arr[i] = sc.nextInt();
-        }
-        Arrays.sort(arr);
+            int [] arr = new int [N];
 
-        System.out.println(calcuteMean(arr));
+            for(int j=0;j<N;j++){
+                arr[j] = sc.nextInt();
+            }
 
-        System.out.println(arr[N/2]);
-
-        System.out.println(findMode(arr));
-
-        System.out.println(arr[N-1] - arr[0]);
-    }
-
-    public static int calcuteMean(int[] arr){
-        double sum = Arrays.stream(arr).sum();
-        double mean = sum / arr.length;
-
-        if(mean<0.0){
-            return (int)Math.round(Math.abs(mean)*-1);
-        }
-        else{
-            return (int)Math.round(mean);
-        }
-    }
-
-    public static int findMode(int[] arr) {
-        Map<Integer, Long> frequencyMap = Arrays.stream(arr)
-                .boxed()
-                .collect(Collectors.groupingBy(e -> e, Collectors.counting()));
-
-        long maxFrequency = frequencyMap.values().stream()
-                .max(Long::compare)
-                .orElseThrow();
-
-        if (frequencyMap.values().stream().filter(freq -> freq == maxFrequency).count() > 1) {
-            int result = frequencyMap.entrySet().stream()
-                    .filter(entry -> entry.getValue() == maxFrequency)
-                    .map(Map.Entry::getKey)
-                    .sorted()
-                    .skip(1)
-                    .findFirst()
-                    .orElseThrow();
-            return result;
-        } else {
-            return frequencyMap.entrySet().stream()
-                    .max(Map.Entry.comparingByValue())
-                    .map(Map.Entry::getKey)
-                    .orElseThrow();
+            for(int j=0;j<N;j++){
+                if(j<M){
+                    if(arr[j] >=  arr[M]){
+                        turn++;
+                    }
+                }
+                if(M<j){
+                    if(arr[j] >= arr[M]){
+                        turn++;
+                        for(int k = j;k<N;k++){
+                            if(arr[k]>arr[j]){
+                                turn--;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            System.out.println(turn);
         }
     }
 }
