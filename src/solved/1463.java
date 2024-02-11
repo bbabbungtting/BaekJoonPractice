96a25acf0516c4fe795864d79d3508fec7504e19
Jaewon <choijawon99@gmail.com>
2024-02-11 14:48:29 +0900

    [FEAT] 2/11 백준 2문제

--- src/solved/1463.java
+++ src/solved/1463.java
@@ -1,8 +1,9 @@
-package org.example;
+//백준 1463번 '1로 만들기' / 실버 3 / 200분 / DP
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,7 +11,14 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-
+        System.out.println(DP(N, 0));
     }
 
+    static int DP(int N, int count){
+        if(N<2){
+            return count;
+        }
+
+        return Math.min(DP(N/2,count+1+(N%2)),DP(N/3,count+1+(N%3)));
+    }
 }
