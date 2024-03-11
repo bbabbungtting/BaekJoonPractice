04114156669dabccb1482d7ccf04fc3f90f0333a
Jaewon <choijawon99@gmail.com>
2024-03-11 09:09:08 +0900

    [FEAT] 3/10 백준 3문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,60 @@
 package org.example;
 
-import java.util.*;
+//백준 14247번 '나무 자르기' / 실버 2 / 40분
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int [] treeLength;
+    static int [] growLength;
+
     public static void main(String[] args) throws IOException {
-        HashMap<String, Integer> map = new HashMap<>();
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for(int i=0;i<N;i++){
-            String key = st.nextToken();
-            map.put(key,map.getOrDefault(key,0)+1);
+
+        treeLength = new int [N];
+        growLength = new int [N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            treeLength[i] = Integer.parseInt(st.nextToken());
         }
 
-        int M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            sb.append(map.getOrDefault(st.nextToken(),0)).append(' ');
+        for (int i = 0; i < N; i++) {
+            growLength[i] = Integer.parseInt(st.nextToken());
         }
-        System.out.println(sb);
+
+        //Main Logic
+        int length = 0;
+        int max = -1;
+        int maxIndex = -1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < treeLength.length; j++) {
+                if(treeLength[j] > max){
+                    max = treeLength[j];
+                    maxIndex = j;
+                }
+            }
+            length+=max;
+            treeLength[maxIndex] = 0;
+
+            growTree(N);
+
+        }
+
+        System.out.println(length);
     }
+
+    static void growTree(int N){
+        for (int i = 0; i < N; i++) {
+            treeLength[i] += growLength[i];
+        }
+    }
+
+
 }
